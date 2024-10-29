c866d2e748a6ee6edcfd309d032cca3628d4e477
Bálint Juhász <jbebe@users.noreply.github.com>
2024-10-29 12:26:55 +0100

    Finally builds

--- Launcher/Launcher/App.cs
+++ Launcher/Launcher/App.cs
@@ -1,7 +1,19 @@
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System;
 using System.Windows;
 
 namespace Launcher;
 
 public partial class App : Application
 {
+    [STAThread]
+    [DebuggerNonUserCode]
+    [GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
+    public static void Main()
+    {
+        App app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
 }

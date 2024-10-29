db079f3b76fa85a18c5ec446a789891be5419050
Bálint Juhász <jbebe@users.noreply.github.com>
2024-10-29 16:33:02 +0100

    Fix runtime

--- Launcher/Launcher/App.cs
+++ Launcher/Launcher/App.cs
@@ -12,6 +12,11 @@ public partial class App : Application
     [GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
     public static void Main()
     {
+#if DEBUG
+        // We need to hook on the instance launched by Steam to get a valid app context.
+        // This message box waits for VS to attach the debugger then we can start debugging.
+        MessageBox.Show("Enter Launcher in debug mode");
+#endif
         App app = new App();
         app.InitializeComponent();
         app.Run();
